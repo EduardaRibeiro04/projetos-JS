be135370e369e504ee21e80489a74bff8590f639
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-06-20 18:56:47 -0300

    operadores

---
+++ 8_boolean_op_logico/scripts.js
@@ -0,0 +1,8 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+console.log(3 == 3 && 'matheus' == 'matheus');
+console.log('filipe' == 'joão' || false);
+console.log(!(true && true));
+
+console.log(true && true);
+console.log(true || false);
---
+++ 9_boolean_op_ternario/scripts.js
@@ -0,0 +1,3 @@
+console.log(5 > 2 ? 'é sim' : 'é não');
+console.log(false ? 5 : 4);
+console.log('duda' == 'duda' ? 'olá, duda!' : 'voçê não é a duda')
