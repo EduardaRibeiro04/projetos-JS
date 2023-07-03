c63fdea9ef263037f57b9f113ce4898f63fe6bd5
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-07-03 19:27:01 -0300

    criando pastas

---
+++ introdução_2/1_variavel/scripts.js
@@ -0,0 +1,18 @@
+let nome = "duda";
+
+console.log(nome);
+
+console.log(`meu nome é ${nome}`)
+
+let laranjas = 5;
+let abacaxis = 10;
+
+console.log(laranjas * laranjas);
+
+console.log(laranjas + abacaxis);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log(um + dois + tres);
+
+
---
+++ introdução_2/2_variaveis_const_var/scripts.js
@@ -0,0 +1,7 @@
+var nome = "duda";
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
+
+
