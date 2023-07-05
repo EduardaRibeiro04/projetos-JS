1303d151cd4745d4871324d978d3fc30e2d1c026
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-07-05 19:50:26 -0300

    exercicio 9

---
+++ exercicios-js/exercicio 9/scripts.js
@@ -0,0 +1,13 @@
+let a = Math.pow(2,2);
+let b = Math.pow(3,2);
+let c = Math.pow(18,2);
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+const num = 18;
+
+console.log(Math.pow(2,2));
+console.log(Math.pow(3,2));
+console.log(Math.pow(num,2));
