48ba6b4eae6be5797fccb0ae612b9b364d4a51b0
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-07-05 21:00:41 -0300

    exercicio 11

---
+++ exercicios-js/exercicio 11/scripts.js
@@ -0,0 +1,10 @@
+let idade = 19;
+let cnh = true;
+
+if(idade >= 18 && !(true)) {
+    console.log("voçê já pode tirar a sua cnh");
+} else if(idade >= 18 && true) {
+    console.log("voçê já dirige!");
+} else {
+    console.log("voçê não pode dirigir!")
+}
