f127eb8f01886382f6b174ebed6baf643c1e3ed2
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-07-10 20:49:27 -0300

    exercicio 14

---
+++ exercicios-js/exercicio 14/scripts.js
@@ -0,0 +1,9 @@
+for(let i = 0; i <= 50; i++) {
+    console.log(i)
+
+    if(i % 2 == 0) {
+        console.log(`o número ${i} é par`);
+    } else {
+        console.log(`o numero ${i} é impar`);
+    }
+}
