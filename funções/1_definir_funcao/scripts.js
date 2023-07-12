5c4003176172fc15318b6ee64c270d04e28bb293
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-07-12 19:41:42 -0300

    definindo funçoes

--- funções/1_definir_funcao/scripts.js
+++ funções/1_definir_funcao/scripts.js
@@ -1,3 +1,22 @@
 function imprimirNoConsole() {
-    console.log("olá, mundo!")
+    console.log("olá, mundo!");
 }
+
+imprimirNoConsole();
+
+function imprimirUmNumero(num) {
+    console.log("o número é: " + num);
+}
+
+imprimirUmNumero(2);
+imprimirUmNumero(4);
+imprimirUmNumero(6);
+imprimirUmNumero(8);
+
+const numeroAleatorio = function() {
+    console.log(Math.random());
+}
+
+numeroAleatorio();
+numeroAleatorio();
+
