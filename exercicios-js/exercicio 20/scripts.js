6fea1520576ddc257148c3a5820249b6fcbd3177
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-08-01 19:53:03 -0300

    exercicio 20

---
+++ exercicios-js/exercicio 20/scripts.js
@@ -0,0 +1,11 @@
+function idade(a) {
+    if(a >= 18) {
+        console.log("voçê pode dirigir")
+    } else {
+        console.log("voçê ainda não pode dirigir")
+    }
+}
+
+idade(18);
+idade(16);
+idade(34);
