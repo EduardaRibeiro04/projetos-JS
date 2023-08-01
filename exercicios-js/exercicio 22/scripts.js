19b7ca50f2f4f7dd89fb3a47bddc0d02fd4c24e6
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-08-01 20:44:09 -0300

    exercicio 22

---
+++ exercicios-js/exercicio 22/scripts.js
@@ -0,0 +1,9 @@
+function retornarValor(numNegativo) {
+    return Math.abs(numNegativo);
+   
+}
+
+console.log(retornarValor(-13));
+console.log(retornarValor(5));
+console.log(retornarValor(-10));
+
