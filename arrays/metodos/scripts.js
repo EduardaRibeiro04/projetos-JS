4cdd4add4a2b64d71060b6b626a621c40ec8ba4e
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-08-10 20:00:26 -0300

    metodos

---
+++ arrays/metodos/scripts.js
@@ -0,0 +1,12 @@
+let marca = 'nike';
+
+console.log(marca.toLocaleUpperCase());
+
+let marca2 = marca.toLocaleUpperCase();
+
+console.log(marca2.toLowerCase());
+
+console.log(typeof marca2.toLowerCase);
+
+//string.propriedade
+//string.metodo()
