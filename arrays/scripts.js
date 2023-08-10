0c53cb9b0d31e041bf5f2091417ed03d5fb2bb57
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-08-10 18:49:48 -0300

    introdução array

---
+++ arrays/scripts.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5];
+let nomes = ['duda', 'douglas', 'sophia'];
+let bool = [true, false, true];
+
+let misturado = [2, 'duda', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
