226e08a1a0dcdbfb84f73e2ad101ca99c4b950ab
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-08-10 19:10:03 -0300

    array 2

---
+++ arrays/arrays_2/scripts.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5];
+let nomes = ['duda', 'douglas', 'sophia'];
+let bool = [true, false, true];
+
+
+
+console.log(arr[1]);
+console.log(nomes[0]);
+console.log(bool[2]);
+console.log(arr[arr.length - 1]);
