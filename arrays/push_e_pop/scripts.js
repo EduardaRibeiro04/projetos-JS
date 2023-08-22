5359daaa596374373126bf5b407779e43c325275
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-08-22 20:41:53 -0300

    push e pop

---
+++ arrays/push_e_pop/scripts.js
@@ -0,0 +1,13 @@
+let nomes = ["duda", "douglas", "eloa"];
+
+let elementoremovido = nomes.pop();
+
+console.log(elementoremovido);
+console.log(nomes);
+
+nomes.push('madu');
+
+console.log(nomes);
+
+//pop = remove o ultimo elemento do array
+//push = adiciona o ultimo elemento do array
