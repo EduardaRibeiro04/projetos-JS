35b93ca3561b8033553ffd13796424dd892520ac
Maria Eduarda De Lima Ribeiro <130518856+EduardaRibeiro04@users.noreply.github.com>
2023-08-22 20:51:45 -0300

    shift e unshift

---
+++ arrays/shift_e_unshift/scripts.js
@@ -0,0 +1,11 @@
+let carros = ["bmw", "porche", "audi"];
+
+let  removerPrimeiroCarro = carros.shift();
+
+console.log(removerPrimeiroCarro);
+console.log(carros);
+
+carros.unshift('duster');
+
+console.log(carros);
+console.log(carros[0]);
